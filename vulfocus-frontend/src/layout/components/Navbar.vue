83b1fb17e1d31b9dbfecb6bfd9d7b8bcb343b243
Liiuliu <992343696@qq.com>
2021-08-08 21:00:56 +0800

    update code

--- vulfocus-frontend/src/layout/components/Navbar.vue
+++ vulfocus-frontend/src/layout/components/Navbar.vue
@@ -59,6 +59,7 @@ import Breadcrumb from '@/components/Breadcrumb'
 import Hamburger from '@/components/Hamburger'
 import { lininfo } from "@/api/docker"
 import { updatePassword } from "@/api/user"
+
 export default {
   data(){
     const validatePass = (rule, value, callback) => {
@@ -152,6 +153,7 @@ export default {
   position: relative;
   background: #fff;
   box-shadow: 0 1px 4px rgba(0,21,41,.08);
+
   .hamburger-container {
     line-height: 46px;
     height: 100%;
@@ -159,20 +161,25 @@ export default {
     cursor: pointer;
     transition: background .3s;
     -webkit-tap-highlight-color:transparent;
+
     &:hover {
       background: rgba(0, 0, 0, .025)
     }
   }
+
   .breadcrumb-container {
     float: left;
   }
+
   .right-menu {
     float: right;
     height: 100%;
     line-height: 50px;
+
     &:focus {
       outline: none;
     }
+
     .right-menu-item {
       display: inline-block;
       padding: 0 8px;
@@ -180,25 +187,31 @@ export default {
       font-size: 18px;
       color: #5a5e66;
       vertical-align: text-bottom;
+
       &.hover-effect {
         cursor: pointer;
         transition: background .3s;
+
         &:hover {
           background: rgba(0, 0, 0, .025)
         }
       }
     }
+
     .avatar-container {
       margin-right: 30px;
+
       .avatar-wrapper {
         margin-top: 5px;
         position: relative;
+
         .user-avatar {
           cursor: pointer;
           width: 24px;
           height: 24px;
           border-radius: 10px;
         }
+
         .el-icon-caret-bottom {
           cursor: pointer;
           position: absolute;
--- vulfocus-frontend/src/views/login/index.vue
+++ vulfocus-frontend/src/views/login/index.vue
@@ -126,6 +126,7 @@
 <script>
 import { lininfo } from "@/api/docker"
 import { sendMail,valMail } from "@/api/user"
+
 export default {
   name: 'Login',
   data() {
@@ -272,6 +273,9 @@ export default {
         }
       })
     },
+
+
+
   }
 }
 </script>
@@ -279,20 +283,24 @@ export default {
 <style lang="scss">
 /* 修复input 背景不协调 和光标变色 */
 /* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
+
 $bg:#283443;
 $light_gray:#aaa;
 $cursor: #fff;
+
 @supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
   .login-container .el-input input {
     color: $cursor;
   }
 }
+
 /* reset element-ui css */
 .login-container {
   .el-input {
     display: inline-block;
     height: 48px;
     width: 332px;
+
     input {
       background: transparent;
       border: 0px;
@@ -303,6 +311,7 @@ $cursor: #fff;
       height: 48px;
       width: 332px;
       caret-color: $cursor;
+
       &:-webkit-autofill {
         box-shadow: 0 0 0px 1000px $bg inset !important;
         -webkit-text-fill-color: $cursor !important;
@@ -315,6 +324,7 @@ $cursor: #fff;
   /*.el-dialog__body{*/
   /*  background-color: #3d7ed5;*/
   /*}*/
+
   .el-form-item {
     border: 1px solid rgba(255, 255, 255, 0.1);
     background: rgba(0, 0, 0, 0.1);
@@ -325,6 +335,7 @@ $cursor: #fff;
 </style>
 
 <style lang="scss" scoped>
+
 $bg:#2d3a4b;
 $dark_gray:#889aa4;
 $light_gray:#eee;
@@ -341,16 +352,20 @@ $light_gray:#eee;
     background-image: url("../../assets/loginl.png");
     background-size: 100% 100%;
   }
+
   .tips {
     font-size: 14px;
     color: #fff;
     margin-bottom: 10px;
+
     span {
       &:first-of-type {
         margin-right: 16px;
+
       }
     }
   }
+
   .svg-container {
     padding: 6px 5px 6px 15px;
     color: $dark_gray;
@@ -358,8 +373,10 @@ $light_gray:#eee;
     width: 48px;
     display: inline-block;
   }
+
   .title-container {
     position: relative;
+
     .title {
       font-size: 26px;
       color: $light_gray;
@@ -368,6 +385,7 @@ $light_gray:#eee;
       font-weight: bold;
     }
   }
+
   .show-pwd {
     position: absolute;
     right: 10px;
@@ -389,4 +407,5 @@ $light_gray:#eee;
 }
 </style>
 <style>
+
 </style>
